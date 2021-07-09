74663f121a54e699ee5c1fecdfb4aa69417487a6
Todd LaMothe <todd@Todds-MacBook-Pro.local>
2021-07-09 11:01:29 -0400

    Create backlog table

--- alembic/versions/17e0cf453c00_create_backlog_table.py
+++ alembic/versions/17e0cf453c00_create_backlog_table.py
@@ -17,8 +17,24 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    op.create_table(
+        'backlog',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('name', sa.VARCHAR(length=100), nullable=False),        
+        sa.Column('epic_count', sa.Integer, nullable=False),        
+        sa.Column('total_points', sa.Integer, nullable=False),        
+        sa.Column('points_done', sa.Integer, nullable=False),      
+        sa.Column('points_in_progress', sa.Integer, nullable=False),       
+        sa.Column('points_to_do', sa.Integer, nullable=False),
+        sa.Column('points_percent_complete', sa.Float, nullable=False),
+        sa.Column('total_issues', sa.Integer, nullable=False),
+        sa.Column('issues_done', sa.Integer, nullable=False),
+        sa.Column('issues_in_progress', sa.Integer, nullable=False),
+        sa.Column('issues_to_do', sa.Integer, nullable=False),
+        sa.Column('issues_unestimated', sa.Integer, nullable=False),
+        sa.Column('issues_percent_complete', sa.Float, nullable=False)        
+    )
 
 
 def downgrade():
-    pass
+    op.drop_table('backlog')
