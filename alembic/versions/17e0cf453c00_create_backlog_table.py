732b14e176712cc833bccdc311988b79f78329d8
Todd LaMothe <todd@Todds-MacBook-Pro.local>
2021-07-09 10:21:14 -0400

    Alembic install with empty revision

---
+++ alembic/versions/17e0cf453c00_create_backlog_table.py
@@ -0,0 +1,24 @@
+"""create backlog table
+
+Revision ID: 17e0cf453c00
+Revises: 
+Create Date: 2021-07-09 10:20:45.916263
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '17e0cf453c00'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
